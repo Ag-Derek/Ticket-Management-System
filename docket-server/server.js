8967306d8e03f7bf367486fb99471e92d9a7ae57
Derrick Agorhom <agorhomderrick@gmail.com>
2026-09-03 11:41:48 +0000

    Configure server for Render

--- docket-server/server.js
+++ docket-server/server.js
@@ -25,6 +25,6 @@ app.use('/api/tickets', require('./routes/tickets'));
 app.use('/api/tickets/:ticketId/comments', require('./routes/comments'));
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Docket API listening on http://localhost:${PORT}`);
 });
